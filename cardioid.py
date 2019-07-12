0dbef15c25ffe7f109b9926fa7a269513160b8f9
Declan Warner <dec.warner@gmail.com>
2019-07-12 23:39:54 +0100

    clean up, add functions

--- cardioid.py
+++ cardioid.py
@@ -1,37 +1,49 @@
 from math import pi, sin, cos
 from turtle import pen, up, down, setposition, setheading, circle, clear
 
+
 def polar_coord_to_cartesian(angle, radius):
-    return (radius*cos(angle), radius*sin(angle))
+    x_coord = radius * cos(angle)
+    y_coord = radius * sin(angle)
+    return (x_coord, y_coord)
+
 
 def get_node_angle(node, mod):
-    return (2*node*pi/mod)
+    return (node * 2 * pi) / mod
+
 
 def get_coord_of_node(node, mod, radius):
     angle = get_node_angle(node, mod)
     coord = polar_coord_to_cartesian(angle, radius)
+    return coord
+
+
+def reset_cursor(coord):
+    up()
+    setposition(*coord)
+    down()
+
+
+def reset_canvas(radius):
+    reset_cursor(*polar_coord_to_cartesian(0, radius))
+    setheading(90)
+    clear()
+    circle(radius)
+
 
 def main():
+    radius = 200
     pen(speed=1000, pensize=2)
     cont = "continue"
     while "q" not in cont:
-        clear()
-        radius = 200
         mod = int(input("Modulo: "))
         multiplier = int(input("Multiplier: "))
-        setheading(90)
-        up()
-        setposition(*polar_coord_to_cartesian(0, radius))
-        down()
-        circle(radius)
+        reset_canvas(radius)
         for node in range(mod):
-            up()
             start = get_coord_of_node(node, mod, radius)
             end = get_coord_of_node(node*multiplier, mod, radius)
-            setposition(*start)
-            down()
+            reset_cursor(*start)
             setposition(*end)
-    
         cont = input("q to quit: ")
 
 
