13cbe3a5acaf76321b22cc5251b14dc4ce53c208
Declan Warner <dec.warner@gmail.com>
2019-07-12 22:14:24 +0100

    Update cardioid.py

--- cardioid.py
+++ cardioid.py
@@ -1,31 +1,34 @@
 from math import pi, sin, cos
 from turtle import pen, up, down, setposition, setheading, circle, clear
 
-def get_coord_from_angle(angle, scale):
-    return (scale*cos(angle), scale*sin(angle))
+def polar_coord_to_cartesian(angle, radius):
+    return (radius*cos(angle), radius*sin(angle))
 
 def get_node_angle(node, mod):
     return (2*node*pi/mod)
+
+def get_coord_of_node(node, mod, radius):
+    angle
 
 def main():
     pen(speed=1000, pensize=2)
     cont = "continue"
     while "q" not in cont:
         clear()
-        scale = 200
+        radius = 200
         mod = int(input("Modulo: "))
         multiplier = int(input("Multiplier: "))
         setheading(90)
         up()
-        setposition(get_coord_from_angle(0, scale))
+        setposition(get_coord_from_angle(0, radius))
         down()
-        circle(scale)
+        circle(radius)
         for node in range(mod):
             up()
             angle = get_node_angle(node,mod)
             next_angle = get_node_angle(node*multiplier, mod)
-            start = get_coord_from_angle(angle, scale)
-            end = get_coord_from_angle(next_angle, scale)
+            start = get_coord_from_angle(angle, radius)
+            end = get_coord_from_angle(next_angle, radius)
             setposition(*start)
             down()
             setposition(*end)
