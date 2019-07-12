a3cb4e715078493e5c5b6a015df1cd22ff340143
Declan Warner <dec.warner@gmail.com>
2019-07-12 23:26:54 +0100

    Update cardioid.py

--- cardioid.py
+++ cardioid.py
@@ -8,7 +8,8 @@ def get_node_angle(node, mod):
     return (2*node*pi/mod)
 
 def get_coord_of_node(node, mod, radius):
-    angle
+    angle = get_node_angle(node, mod)
+    coord = polar_coord_to_cartesian(angle, radius)
 
 def main():
     pen(speed=1000, pensize=2)
@@ -20,15 +21,13 @@ def main():
         multiplier = int(input("Multiplier: "))
         setheading(90)
         up()
-        setposition(get_coord_from_angle(0, radius))
+        setposition(*polar_coord_to_cartesian(0, radius))
         down()
         circle(radius)
         for node in range(mod):
             up()
-            angle = get_node_angle(node,mod)
-            next_angle = get_node_angle(node*multiplier, mod)
-            start = get_coord_from_angle(angle, radius)
-            end = get_coord_from_angle(next_angle, radius)
+            start = get_coord_of_node(node, mod, radius)
+            end = get_coord_of_node(node*multiplier, mod, radius)
             setposition(*start)
             down()
             setposition(*end)
