266e3e28341be4777d42cfdc10612c4e52bb6466
Declan Warner <dec.warner@gmail.com>
2019-07-13 23:41:47 +0100

    add js svg version

--- theory.py
+++ theory.py
@@ -5,6 +5,20 @@
 # for p = 2s
 # k-1= 2mod/k
 # mod = k*(k-1)/2
-for k in range(1, 100):
-    input(str(k) + ": " + str(k*(k-1)/2))
+
+def map(num):
+    return num*(num-1)
+
+
+def mess(num):
+    return str(num) + ": "  + str(map(num))
+
+
+ks = range(1, 1000)
+
+messages = (mess(k) for k in ks)
+
+for message in messages:
+    input(message)
+
 
